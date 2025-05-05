978202dfc2f4689ba2030b2d0ef7a04c16e36163
Thomas Niedermaier <thomas.niedermaier@gmail.com>
2025-05-05 16:34:54 +0200

    Update version.php to require Moodle 4.1+

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'customfield_semester';
-$plugin->version   = 2025043000;
-$plugin->requires  = 2024100700;
-$plugin->release = 'v4.5-r1';
+$plugin->version   = 2025043001;
+$plugin->requires  = 2022112800;
+$plugin->release = 'v4.5-r2';
 $plugin->supported = [401, 405];
 $plugin->maturity = MATURITY_STABLE;
