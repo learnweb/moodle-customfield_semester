dd9d663470707799107f51518778eac6a3d9b2e3
lenaherf <lherfeld@uni-muenster.de>
2026-06-22 15:53:28 +0200

    another fix to try

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'customfield_semester';
 $plugin->version   = 2026061700;     // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2024100712;     // Requires 4.5+ Moodle version.
+$plugin->requires  = 2026042001;     // Requires 5.2+ Moodle version.
 $plugin->release = 'v5.2-r1';
-$plugin->supported = [502 => 502]; // Moodle 5.2.
+$plugin->supported = [502]; // Moodle 5.2.
 $plugin->maturity = MATURITY_RC;
